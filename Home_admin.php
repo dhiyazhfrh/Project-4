10526db97e58a03dd73a562e2cadb28798792290
surya-rifqi <suryarifqi21@gmail.com>
2021-06-30 20:32:14 +0700

    .

--- Home_admin.php
+++ Home_admin.php
@@ -12,7 +12,7 @@
             <img class="logo" src="img/logotxt.png">
             <nav>
                 <ul class="nav__links">
-                    <li><a href="Class_admin.php">Class</a></li>
+                    <li><a href="View_admin.php">Class</a></li>
                     <li><a href="#">Student</a></li>
                     <li><a href="#">Profile</a></li>
                     <li><a href="#">Manage</a></li>
