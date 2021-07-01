e81a661e31e7749e23e1ad7cdf4ca694896c3875
dhiyazhfrh <dhiyazhafirah86@gmail.com>
2021-07-01 20:37:57 +0700

    update

--- Input_manage.php
+++ Input_manage.php
@@ -1,42 +1,37 @@
-<?php
-include "koneksi.php";
-session_start();
-if (!isset($_SESSION['username'])){
-    header('location:login.php');
-}
-?>
 <html>
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <title>Home</title>
-        <link rel="stylesheet" type="text/css" href="Input_class.css">
+        <link rel="stylesheet" type="text/css" href="Input_manage.css">
         <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet"> 
         <meta name="viewport" content="width=device-width, initial-scale=1">
     </head>
     <body>
         <div class="container">
             <div class="input">
-                <a href="View_class_admin.php"> < Back</a>
-                <h5>New Class</h5>
+                <a href="View_manage_admin.php"> < Back</a>
+                <h5>New User</h5>
                 <form action="#input" name="schedule" method="post">
                     <div class="input-1">
-                        <h2>Class Name</h2>
-                        <input type="text" class="class_name">
+                        <h2>Username</h2>
+                        <input type="text" class="username">
                     </div>
                     <div class="input-1">
-                        <h2>Grade</h2>
-                        <input type="text" class="grade">
+                        <h2>Full Name</h2>
+                        <input type="text" class="full_name">
                     </div>
                     <div class="input-1">
-                        <h2>Room</h2>
-                        <select name="room" class="room">
-                            <option value="">-- Choose room --</option></select>
+                        <h2>Email</h2>
+                        <input type="email" class="mail">
                     </div>
                     <div class="input-1">
-                        <h2>Schedule</h2>
-                        <select name="time" class="time">
-                            <option value="">-- Choose time --</option></select>
+                        <h2>Phone Number</h2>
+                        <input type="number" class="phone">
+                    </div>
+                    <div class="input-1">
+                        <h2>Position</h2>
+                        <input type="text" class="post">
                     </div>
                     <input type="submit" class="btn" value="Save">
                 </form>
