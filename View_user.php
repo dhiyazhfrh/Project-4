3eba1ef9f8bb044232e0b790e217f81dccca3059
dhiyazhfrh <dhiyazhafirah86@gmail.com>
2021-07-01 16:09:15 +0700

    update

--- View_user.php
+++ View_user.php
@@ -20,7 +20,7 @@ if (!isset($_SESSION['username'])){
             <nav>
                 <ul class="nav__links">
                     <li><a href="View_user.php">Class</a><div class="rec"></div></li>
-                    <li><a href="#">Student</a></li>
+                    <li><a href="View_student_user.php">Student</a></li>
                     <li><a href="#">Profile</a></li>
                     <li><a href="logout.php">Log out</a></li>
                 </ul>
