84c8eff0bb489e2d860d743adee2f5c9b5addd64
dhiyazhfrh <dhiyazhafirah86@gmail.com>
2021-07-01 14:49:04 +0700

    update

--- View_student_admin.php
+++ View_student_admin.php
@@ -12,8 +12,8 @@
             <img class="logo" src="img/logotxt.png">
             <nav>
                 <ul class="nav__links">
-                    <li><a href="View_admin.php">Class</a><div class="rec"></div></li>
-                    <li><a href="View_student_admin.php">Student</a></li>
+                    <li><a href="View_admin.php">Class</a></li>
+                    <li><a href="View_student_admin.php">Student</a><div class="rec"></div></li>
                     <li><a href="#">Profile</a></li>
                     <li><a href="#">Manage</a></li>
                     <li><a href="logout.php">Log out</a></li>
