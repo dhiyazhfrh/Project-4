a6b78bcac5b0fdd31030075d6b7c511d16c94f70
surya-rifqi <suryarifqi21@gmail.com>
2021-07-01 21:04:14 +0700

    .

--- delete_room.php
+++ delete_room.php
@@ -2,10 +2,9 @@
 include "koneksi.php";
 $room_id = isset($_GET['id'])? $_GET['id']:'';
 
-$query = mysqli_query($connect, "DELETE FROM room WHERE id = '$room_id'");
+$query = mysqli_query($connect, "DELETE FROM room WHERE room_id = '$room_id'");
 if ($query){
     header('location:View_room_admin.php');
-    echo "<script>alert('Room Deleted')</script>";
 }
 
 ?>
