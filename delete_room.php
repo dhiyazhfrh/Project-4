5a64af34e35106af7be99bb5cd4a4e241b076932
surya-rifqi <suryarifqi21@gmail.com>
2021-07-01 21:17:46 +0700

    .

--- delete_room.php
+++ delete_room.php
@@ -5,6 +5,7 @@ $room_id = isset($_GET['id'])? $_GET['id']:'';
 $query = mysqli_query($connect, "DELETE FROM room WHERE room_id = '$room_id'");
 if ($query){
     header('location:View_room_admin.php');
+    echo "<script>alert('Room deleted')</script>";
 }
 
 ?>
