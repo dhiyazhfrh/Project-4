ba3a9b5d2e719667e12701baac542ed065b9b662
dhiyazhfrh <dhiyazhafirah86@gmail.com>
2021-07-01 15:48:34 +0700

    update

--- View_schedule_user.php
+++ View_schedule_user.php
@@ -1,52 +1,42 @@
-<?php
-include "koneksi.php";
-session_start();
-if (!isset($_SESSION['username'])){
-    header('location:login.php');
-}
-?>
 <html>
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <title>Home</title>
-        <link rel="stylesheet" type="text/css" href="View_class_user.css">
+        <link rel="stylesheet" type="text/css" href="View_schedule_user.css">
         <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet"> 
         <meta name="viewport" content="width=device-width, initial-scale=1">
     </head>
     <body>
         <header>
-            <img class="logo" src="img/logotxt.png">
+            <img class="logo" src="img/logotxt.png" href="Home_admin.php">
             <nav>
                 <ul class="nav__links">
-                    <li><a href="View_user.php">Class</a><div class="rec"></div></li>
-                    <li><a href="View_student_user.php">Student</a></li>
+                    <li><a href="View_admin.php">Class</a><div class="rec"></div></li>
+                    <li><a href="View_student_admin.php">Student</a></li>
                     <li><a href="#">Profile</a></li>
+                    <li><a href="#">Manage</a></li>
                     <li><a href="logout.php">Log out</a></li>
                 </ul>
             </nav>
         </header>
         <div class="title">
-            <img class="schedule" src="img/schedule.png">
-            <h2>Class</h2>
-        </div>
+            <img class="time" src="img/time.png">
+            <h2>Schedule</h2>
             <div class="container">
             <table class="content-table">
                 <thead>
                 <tr>
-                    <th>Class Name</th>
-                    <th>Grade</th>
-                    <th>Room</th>
+                    <th>Day</th>
                     <th>Time</th>
                 </tr>
                 </thead>
                 <tbody>
                     <tr>
-                      <td>Pre A</td>
-                      <td>Preschool</td>
-                      <td>#</td>
-                      <td>#</td>
+                      <td>Tuesday</td>
+                      <td>10.00 - 12.00</td>
                     </tr>
+                <tbody>
             </table>
         </div>
     </body>
