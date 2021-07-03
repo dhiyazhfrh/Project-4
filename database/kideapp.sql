0cba03fb1b929c9889033f8731afb62f47755764
surya-rifqi <suryarifqi21@gmail.com>
2021-07-03 10:59:59 +0700

    class tabel

--- database/kideapp.sql
+++ database/kideapp.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 02, 2021 at 04:50 PM
+-- Generation Time: Jul 03, 2021 at 05:59 AM
 -- Server version: 5.5.32
 -- PHP Version: 5.4.19
 
@@ -25,6 +25,49 @@ USE `kideapp`;
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `class`
+--
+
+CREATE TABLE IF NOT EXISTS `class` (
+  `class_id` int(11) NOT NULL AUTO_INCREMENT,
+  `room_id` int(11) NOT NULL,
+  `time_id` int(11) NOT NULL,
+  PRIMARY KEY (`class_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `class`
+--
+
+INSERT INTO `class` (`class_id`, `room_id`, `time_id`) VALUES
+(1, 9, 6);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `class_detail`
+--
+
+CREATE TABLE IF NOT EXISTS `class_detail` (
+  `detail_id` int(11) NOT NULL AUTO_INCREMENT,
+  `class_name` varchar(20) NOT NULL,
+  `grade` varchar(15) NOT NULL,
+  `class_id` int(11) NOT NULL,
+  `room_id` int(11) NOT NULL,
+  `time_id` int(11) NOT NULL,
+  PRIMARY KEY (`detail_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `class_detail`
+--
+
+INSERT INTO `class_detail` (`detail_id`, `class_name`, `grade`, `class_id`, `room_id`, `time_id`) VALUES
+(1, 'Pre A', 'Preschool', 1, 9, 6);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `room`
 --
 
@@ -33,14 +76,15 @@ CREATE TABLE IF NOT EXISTS `room` (
   `room_name` varchar(30) NOT NULL,
   `capacity` int(11) NOT NULL,
   PRIMARY KEY (`room_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10 ;
 
 --
 -- Dumping data for table `room`
 --
 
 INSERT INTO `room` (`room_id`, `room_name`, `capacity`) VALUES
-(7, 'sample1', 23);
+(8, 'sample2', 23),
+(9, 'sample4', 25);
 
 -- --------------------------------------------------------
 
@@ -55,7 +99,15 @@ CREATE TABLE IF NOT EXISTS `tbl_time` (
   `start_time` time NOT NULL,
   `end_time` time NOT NULL,
   PRIMARY KEY (`time_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+
+--
+-- Dumping data for table `tbl_time`
+--
+
+INSERT INTO `tbl_time` (`time_id`, `code`, `day_time`, `start_time`, `end_time`) VALUES
+(5, 'MON1012', 'Monday', '22:29:00', '00:29:00'),
+(6, 'TUES1112', 'Tuesday', '00:20:00', '12:20:00');
 
 -- --------------------------------------------------------
 
