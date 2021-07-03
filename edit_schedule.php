9f3a65ff54c75d137af89f27ccd541ef3520fa87
dhiyazhfrh <dhiyazhafirah86@gmail.com>
2021-07-03 11:41:14 +0700

    update edit schedule

--- edit_schedule.php
+++ edit_schedule.php
@@ -1,29 +1,9 @@
-<?php
-include "koneksi.php";
-session_start();
-if (!isset($_SESSION['username'])){
-    header('location:login.php');
-}
-?>
 <html>
     <head>
-        <style>
-            .cod{
-    padding: 4px;
-    opacity: 80%;
-    width: 100%;
-    height: 40px;
-    background-color: #f4cba7;
-    border-radius: 10px;
-    color: white;
-    font-size: 16px;
-	font-family: 'Poppins', sans-serif;
-}
-        </style>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <title>Home</title>
-        <link rel="stylesheet" type="text/css" href="Input_schedule.css">
+        <link rel="stylesheet" type="text/css" href="edit_schedule.css">
         <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet"> 
         <meta name="viewport" content="width=device-width, initial-scale=1">
     </head>
@@ -31,20 +11,28 @@ if (!isset($_SESSION['username'])){
         <div class="container">
             <div class="input">
                 <a href="View_schedule_admin.php"> < Back</a>
-                <h5>New Schedule</h5>
-                <form action="Input_schedule_process.php" method="post">
-                    <div class="input-1">
-                        <h2>Code</h2>
-                        <?php
-                            $id = isset($_GET['id'])? $_GET['id']:'';
+                <h5>Edit Schedule</h5>
+                <?php
+                
+                    $id = isset($_GET['time_id'])? $_GET['time_id']:'';
 
-                            $query = mysqli_query($connect, "SELECT * FROM tbl_time WHERE time_id = '$id'");
-                            $time = mysqli_fetch_array($query);
+                    $query = mysqli_query($connect, "SELECT * FROM tbl_time WHERE time_id = '$id'");
+                    $time = mysqli_fetch_array($query);
 
-                        ?>
-                        <input type="text" name="code1" class="cod" value="<?php echo $time['code'];?>"><b>
-                    <div class="input-1">
-                        <h2>Day</h2>
+                ?>
+                
+                <form action="Input_schedule_process.php" method="POST">
+                <table class="input-1">
+                    <tr>
+                        <td><h2>Code</h2></td>
+                        <td>
+                            <input type="text" name="code1" class="code" value="<?php echo $time['code']; ?>">
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td><h2>Day</h2></td>
+                        <td>
                         <select name="day" class="day" value ="<?php echo $time['day_time'];?>">
                             <option value="Monday">Monday</option>
                             <option value="Tuesday">Tuesday</option>
@@ -52,12 +40,23 @@ if (!isset($_SESSION['username'])){
                             <option value="Thursday">Thursday</option>
                             <option value="Friday">Friday</option>
                         </select>
-                    </div>
-                    <div class="input-1">
-                        <h2>Time</h2>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td><h2>Time</h2></td>
+                        <td>
                         <input type="time" name="start" class="time" value="<?php echo $time['start_time'];?>"><b> - </b> <input type="time" name="end" class="time" value="<?php echo $time['end_time'];?>">
-                    </div>
-                    <input type="submit" class="btn" value="Save">
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td colspan="2">
+                            <input type="hidden" name="id" value="<?php echo $id; ?>">
+                            <input type="submit" name="submit" value="Update" class="btn">
+                        </td>
+                    </tr>
+                </table>
                 </form>
             </div>
         </div>
