3e829848eeee05d35b021ab42c903268e907a454
dhiyazhfrh <dhiyazhafirah86@gmail.com>
2021-07-03 11:46:36 +0700

    update room

--- edit_room.php
+++ edit_room.php
@@ -1,36 +1,49 @@
-<?php
-include "koneksi.php";
-session_start();
-if (!isset($_SESSION['username'])){
-    header('location:login.php');
-}
-?>
 <html>
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>Home</title>
-        <link rel="stylesheet" type="text/css" href="Input_room.css">
+        <title>Room</title>
+        <link rel="stylesheet" type="text/css" href="edit_schedule.css">
         <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet"> 
         <meta name="viewport" content="width=device-width, initial-scale=1">
     </head>
     <body>
         <div class="container">
             <div class="input">
-                <a href="View_room_admin.php"> < Back</a>
-                <h5>New Room</h5>
-                <form action="Input_room_process.php" name="room" method="post">
-                    <div class="input-1">
-                        <h2>Room</h2>
-                        <input type="text" name="room_name" class="room">
-                    </div>
-                    <div class="input-1">
-                        <h2>Capacity</h2>
-                        <input type="number" name="room_cap" class="capacity">
-                    </div>
-                    <input type="submit" class="btn" value="Save">
+                <a href="View_schedule_admin.php"> < Back</a>
+                <h5>Edit Room</h5>
+                <?php
+                
+                    $id = isset($_GET['time_id'])? $_GET['time_id']:'';
+
+                    $query = mysqli_query($connect, "SELECT * FROM tbl_time WHERE time_id = '$id'");
+                    $time = mysqli_fetch_array($query);
+
+                ?>
+                
+                <form action="Input_room_process.php" method="POST">
+                <table class="input-1">
+                    <tr>
+                        <td><h2>Room</h2></td>
+                        <td>
+                            <input type="text" name="code1" class="room" value="<?php echo $time['code']; ?>">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td><h2>Capacity</h2></td>
+                        <td>
+                            <input type="number" name="capacity" class="cap" value="<?php echo $time['code']; ?>">
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td colspan="2">
+                            <input type="hidden" name="id" value="<?php echo $id; ?>">
+                            <input type="submit" name="submit" value="Update" class="btn">
+                        </td>
+                    </tr>
+                </table>
                 </form>
             </div>
         </div>
     </body>
-</html>
